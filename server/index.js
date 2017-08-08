6c808f694edae34d4f878e874f8343b4cf5c6444
Clint Losee <clint.losee@rcwilley.com>
2017-08-08 09:13:07 -0600

    Refactor server and auth files.

--- server/index.js
+++ server/index.js
@@ -1,36 +1,16 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: keys.clientID,
-            clientSecret: keys.clientSecret,
-            callbackURL: '/auth/google/callback'
-        },
-        (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(profile);
-        }
-    )
-);
-
+// basic home route
 app.get('/', (req, res) => {
     res.send('Digital Flash...Yeah!');
 });
 
-app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
+// authentication routes
+require('./routes/authRoutes')(app);
 
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 8888;
 app.listen(PORT, () => {
